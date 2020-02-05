8b3a3a3240cb560e142220c520db4e69092223a9
Yanying Wang <yanyingwang1@gmail.com>
2020-02-06 00:55:13 +0800

    with api

--- info.rkt
+++ info.rkt
@@ -1,8 +1,8 @@
 #lang info
 (define collection "2019-nCov-report")
-(define deps '("base" "html-parsing" "request" "txexpr" "webscraperhelper"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define deps '("base" "at-exp-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "smtpable"))
 (define scribblings '(("scribblings/2019-nCov-report.scrbl" ())))
-(define pkg-desc "Description Here")
-(define version "0.0")
-(define pkg-authors '(yanying))
+(define pkg-desc "2019-nCon SMS reports for China/Henan/Zhengzhou")
+(define version "0.1")
+(define pkg-authors '("Yanying Wang"))
--- main.rkt
+++ main.rkt
@@ -1,97 +1,88 @@
 #!/usr/bin/env racket
-#lang racket/base
+#lang at-exp racket/base
 
-(module+ test
-  (require rackunit))
-
-;; Notice
-;; To install (from within the package directory):
-;;   $ raco pkg install
-;; To install (once uploaded to pkgs.racket-lang.org):
-;;   $ raco pkg install <<name>>
-;; To uninstall:
-;;   $ raco pkg remove <<name>>
-;; To view documentation:
-;;   $ raco docs <<name>>
-;;
-;; For your convenience, we have included LICENSE-MIT and LICENSE-APACHE files.
-;; If you would prefer to use a different license, replace those files with the
-;; desired license.
-;;
-;; Some users like to add a `private/` directory, place auxiliary files there,
-;; and require them in `main.rkt`.
-;;
-;; See the current version of the racket style guide here:
-;; http://docs.racket-lang.org/style/index.html
+#;(module+ test
+    (require rackunit))
 
 
-(require request
-         net/http-client
-         html-parsing
-         xml
-         xml/path
-         racket/file
-         webscraperhelper
-         sxml)
-
-#;(let-values (((status headers in) (http-sendrecv "arademaker.github.io"
-                                                   "/about.html")))
-  (html->xexp in))
-
+(require net/http-client
+         racket/port
+         racket/format
+         json
+         smtpable
+         net/smtp
+         debug/repl)
 
 (define-values (status headers in)
-  (http-sendrecv "news.ifeng.com"
-                 "/c/special/7tPlDSzDgVk"
-                 #:ssl? #t
-                 ;;#:version "1.1"
-                 ;;#:method "GET"
-                 ;;#:data "Hello"
-                 ))
-(define-values (status headers in)
-  (http-sendrecv "m.medsci.cn"
-                 "/wh.asp"
-                 ;;#:ssl? #t
-                 ;;#:version "1.1"
-                 ;;#:method "GET"
-                 ;;#:data "Hello"
-                 ))
+  (http-sendrecv "www.tianqiapi.com"
+                 "/api?version=epidemic&appid=23035354&appsecret=8YvlPNrz"
+                 #:ssl? #t))
 
 #;(displayln status)
 #;(displayln headers)
-#;(displayln (port->string in))
-(define res (html->xexp (port->string in)))
-(sxpath '() res)
+(or (equal? status #"HTTP/1.1 200 OK")
+    (raise "error: api request error"))
+
+(define res (string->jsexpr (port->string in)))
+(define data (hash-ref res 'data))
+
+(define area (hash-ref data 'area))
+(define henan (findf (lambda (i) (equal? (hash-ref i 'provinceName) "河南"))
+                     area))
+
+(define cities (hash-ref henan 'cities))
+(define zhengzhou (findf (lambda (i) (equal? (hash-ref i 'cityName) "郑州"))
+                         cities))
+
+(define content @~a{
+                    全国疑似：@(hash-ref data 'suspect)人，
+                    全国已确诊：@(hash-ref data 'diagnosed)人，
+                    全国已死亡：@(hash-ref data 'death)人，
+                    全国已治愈：@(hash-ref data 'cured)人。
+                    河南疑似：@(hash-ref henan 'suspectedCount)人，
+                    河南已确诊：@(hash-ref henan 'confirmedCount)人，
+                    河南已死亡：@(hash-ref henan 'deadCount)人，
+                    河南已治愈：@(hash-ref henan 'curedCount)人。
+                    郑州疑似：@(hash-ref zhengzhou 'suspectedCount)人，
+                    郑州已确诊：@(hash-ref zhengzhou 'confirmedCount)人，
+                    郑州已死亡：@(hash-ref zhengzhou 'deadCount)人，
+                    郑州已治愈：@(hash-ref zhengzhou 'curedCount)人。
+                    })
+
+(define email (mail "@qq.com" ; sender
+                    '("@139.com") ; recipients
+                    "新型肺炎今日报告" ; subject
+                    content ; content
+                    '() #;attachments))
+
+(debug-repl)
+(smtp-send-message "smtp.qq.com"
+                   (mail-from email)
+                   (mail-tos email)
+                   (mail-header email)
+                   '() ;; note: mail-content already included in mail-header
+                   #:auth-user ""
+                   #:port-no 587
+                   ;; #:tcp-conntect (lambda () (tcp-connect "smtp.qq.com" 587))
+                   #:auth-passwd "")
 
 
-
-#;(class "clearfix placeItem placeArea")
-
-#;(html->xexp "<div class='clearfix placeItem placeArea'>
-<h2 class='blue'>河南</h2>
-<div class='add ac_add' >89</div>
-<div class='confirm'>764</div>
-<div class='heal'>49</div>
-<div class='dead'>2</div>
-</div>")
-(display-to-file (xexp->html res)  "/Users/yanying/abc.html")
-
-
-(module+ test
-  ;; Any code in this `test` submodule runs when this file is run using DrRacket
+#;(module+ test
+;; Any code in this `test` submodule runs when this file is run using DrRacket
   ;; or with `raco test`. The code here does not run when this file is
   ;; required by another module.
 
   (check-equal? (+ 2 2) 4))
 
-(module+ main
-  ;; (Optional) main submodule. Put code here if you need it to be executed when
-  ;; this file is run using DrRacket or the `racket` executable.  The code here
-  ;; does not run when this file is required by another module. Documentation:
-  ;; http://docs.racket-lang.org/guide/Module_Syntax.html#%28part._main-and-test%29
+#;(module+ main
+    ;; (Optional) main submodule. Put code here if you need it to be executed when
+    ;; this file is run using DrRacket or the `racket` executable.  The code here
+    ;; does not run when this file is required by another module. Documentation:
+    ;; http://docs.racket-lang.org/guide/Module_Syntax.html#%28part._main-and-test%29
 
-  (require racket/cmdline)
-  (define who (box "world"))
-  (command-line
+    (require racket/cmdline)
+    (define who (box "world"))
+    (command-line
    #:program "my-program"
    #:once-each
    [("-n" "--name") name "Who to say hello to" (set-box! who name)]
