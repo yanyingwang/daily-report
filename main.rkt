64d94004d6a8520197327f198544f92fe1620bcb
Yanying Wang <yanyingwang1@gmail.com>
2020-02-06 01:15:40 +0800

    remove useless code

--- main.rkt
+++ main.rkt
@@ -1,10 +1,6 @@
 #!/usr/bin/env racket
 #lang at-exp racket/base
 
-#;(module+ test
-    (require rackunit))
-
-
 (require net/http-client
          racket/port
          racket/format
@@ -55,7 +51,6 @@
                     content ; content
                     '() #;attachments))
 
-(debug-repl)
 (smtp-send-message "smtp.qq.com"
                    (mail-from email)
                    (mail-tos email)
@@ -66,25 +61,3 @@
                    ;; #:tcp-conntect (lambda () (tcp-connect "smtp.qq.com" 587))
                    #:auth-passwd "")
 
-
-#;(module+ test
-;; Any code in this `test` submodule runs when this file is run using DrRacket
-  ;; or with `raco test`. The code here does not run when this file is
-  ;; required by another module.
-
-  (check-equal? (+ 2 2) 4))
-
-#;(module+ main
-    ;; (Optional) main submodule. Put code here if you need it to be executed when
-    ;; this file is run using DrRacket or the `racket` executable.  The code here
-    ;; does not run when this file is required by another module. Documentation:
-    ;; http://docs.racket-lang.org/guide/Module_Syntax.html#%28part._main-and-test%29
-
-    (require racket/cmdline)
-    (define who (box "world"))
-    (command-line
-   #:program "my-program"
-   #:once-each
-   [("-n" "--name") name "Who to say hello to" (set-box! who name)]
-   #:args ()
-   (printf "hello ~a~n" (unbox who))))
