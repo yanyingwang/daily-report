b8a374cf21cd62c5ffc0ca1d69448da94d9ded66
Yanying Wang <yanyingwang1@gmail.com>
2020-03-07 22:22:16 +0800

    fix info

--- info.rkt
+++ info.rkt
@@ -1,8 +1,8 @@
 #lang info
-(define collection "2019-nCov-report")
+(define collection "report-covid-19")
 (define deps '("base" "at-exp-lib" "r6rs-lib" "gregor-lib"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "smtpable"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/2019-nCov-report.scrbl" ())))
-(define pkg-desc "2019-nCon SMS reports for China/Henan/Zhengzhou")
+(define pkg-desc "2019-nCon SMS reports")
 (define version "0.1")
 (define pkg-authors '("Yanying Wang"))
