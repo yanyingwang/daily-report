50bafe7b809383ecbe8dd8d0256768e76c10f2bc
Yanying Wang <yanyingwang1@gmail.com>
2020-05-17 20:20:10 +0800

    using smtp

--- info.rkt
+++ info.rkt
@@ -1,8 +1,8 @@
 #lang info
 (define collection "2019-nCov-report")
-(define deps '("base" "at-exp-lib" "r6rs-lib" "gregor-lib"))
+(define deps '("base" "at-exp-lib" "r6rs-lib" "gregor-lib" "smtp"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/2019-nCov-report.scrbl" ())))
-(define pkg-desc "2019-nCon SMS reports")
+(define pkg-desc "2019-nCoV reports")
 (define version "0.1")
 (define pkg-authors '("Yanying Wang"))
--- main.rkt
+++ main.rkt
@@ -6,24 +6,16 @@
          racket/port
          racket/format
          racket/string
-         ;smtp-lib
-	 (file "smtp-lib.rkt")
+         smtp
          json
          ;; debug/repl
          )
 
-(define (do-sending subject content)
-  (define email (mail (getenv "SENDER") ; sender
-                      (string-split (getenv "RECIPIENT")) ; recipients
-                      subject ; subject
-                      content ; content
-                      '() #;attachments))
-  (send-smtp-mail email
-                  #:host "smtp.qq.com"
-                  #:port 587
-                  #:auth-user (getenv "AUTH_USER")
-                  #:auth-passwd (getenv "AUTH_PASSWD")))
 
+(current-smtp-host "smtp.qq.com")
+(current-smtp-port 587)
+(current-smtp-username (getenv "AUTH_USER"))
+(current-smtp-password (getenv "AUTH_PASSWD"))
 
 
 ;;;; sina.cn api
@@ -78,80 +70,59 @@
 
 ;; 新型肺炎今日概览报告
 (define overall @~a{
-                      概览
-                      全国现有确诊：@(hash-ref data 'econNum)人，
-                      全国累计确诊：@(hash-ref data 'gntotal)人，
-                      全国现有疑似：@(hash-ref data 'sustotal)人，
-                      全国现已治愈：@(hash-ref data 'curetotal)人，
-                      全国现已死亡：@(hash-ref data 'deathtotal)人。
-                      河南现有确诊：@(hash-ref henan 'econNum)人，
-                      河南累计确诊：@(hash-ref henan 'value)人，
-                      河南现已治愈：@(hash-ref henan 'cureNum)人，
-                      河南已死亡：@(hash-ref henan 'deathNum)人。
-                      郑州累积确诊：@(hash-ref zhengzhou 'conNum)人，
-                      郑州现已治愈：@(hash-ref zhengzhou 'cureNum)人，
-                      郑州现已死亡：@(hash-ref zhengzhou 'deathNum)人。
-                      })
+                    『概览』
+                    全国现有确诊：@(hash-ref data 'econNum)人，
+                    全国累计确诊：@(hash-ref data 'gntotal)人，
+                    全国现有疑似：@(hash-ref data 'sustotal)人，
+                    全国现已治愈：@(hash-ref data 'curetotal)人，
+                    全国现已死亡：@(hash-ref data 'deathtotal)人。
+                    河南现有确诊：@(hash-ref henan 'econNum)人，
+                    河南累计确诊：@(hash-ref henan 'value)人，
+                    河南现已治愈：@(hash-ref henan 'cureNum)人，
+                    河南已死亡：@(hash-ref henan 'deathNum)人。
+                    郑州累积确诊：@(hash-ref zhengzhou 'conNum)人，
+                    郑州现已治愈：@(hash-ref zhengzhou 'cureNum)人，
+                    郑州现已死亡：@(hash-ref zhengzhou 'deathNum)人。
+                    })
 
 ;; 新型肺炎今日总计排名报告
 (define top5-con @~a{
-                       累积确诊排名
-                       国内前五
-                       @(hash-ref (first sorted-p) 'name)：@(hash-ref (first sorted-p) 'value)人，
-                       @(hash-ref (second sorted-p) 'name)：@(hash-ref (second sorted-p) 'value)人，
-                       @(hash-ref (third sorted-p) 'name)：@(hash-ref (third sorted-p) 'value)人，
-                       @(hash-ref (fourth sorted-p) 'name)：@(hash-ref (fourth sorted-p) 'value)人，
-                       @(hash-ref (fifth sorted-p) 'name)：@(hash-ref (fifth sorted-p) 'value)人。
-                       国外前五
-                       @(hash-ref (first sorted-c) 'name)：@(hash-ref (first sorted-c) 'value)人，
-                       @(hash-ref (second sorted-c) 'name)：@(hash-ref (second sorted-c) 'value)人，
-                       @(hash-ref (third sorted-c) 'name)：@(hash-ref (third sorted-c) 'value)人，
-                       @(hash-ref (fourth sorted-c) 'name)：@(hash-ref (fourth sorted-c) 'value)人，
-                       @(hash-ref (fifth sorted-c) 'name)：@(hash-ref (fifth sorted-c) 'value)人。
-                       })
+                     『累积前五』
+                     @(hash-ref (first sorted-p) 'name)：@(hash-ref (first sorted-p) 'value)人，
+                     @(hash-ref (second sorted-p) 'name)：@(hash-ref (second sorted-p) 'value)人，
+                     @(hash-ref (third sorted-p) 'name)：@(hash-ref (third sorted-p) 'value)人，
+                     @(hash-ref (fourth sorted-p) 'name)：@(hash-ref (fourth sorted-p) 'value)人，
+                     @(hash-ref (fifth sorted-p) 'name)：@(hash-ref (fifth sorted-p) 'value)人。
+                     @(hash-ref (first sorted-c) 'name)：@(hash-ref (first sorted-c) 'value)人，
+                     @(hash-ref (second sorted-c) 'name)：@(hash-ref (second sorted-c) 'value)人，
+                     @(hash-ref (third sorted-c) 'name)：@(hash-ref (third sorted-c) 'value)人，
+                     @(hash-ref (fourth sorted-c) 'name)：@(hash-ref (fourth sorted-c) 'value)人，
+                     @(hash-ref (fifth sorted-c) 'name)：@(hash-ref (fifth sorted-c) 'value)人。
+                     })
 
 
 (define top5-con-today @~a{
-                             新增确诊排名
-                             国内前五
-                             @(hash-ref (first sorted-daily-p) 'name)：@(hash-ref (first sorted-daily-p) 'value)人，
-                             @(hash-ref (second sorted-daily-p) 'name)：@(hash-ref (second sorted-daily-p) 'value)人，
-                             @(hash-ref (third sorted-daily-p) 'name)：@(hash-ref (third sorted-daily-p) 'value)人，
-                             @(hash-ref (fourth sorted-daily-p) 'name)：@(hash-ref (fourth sorted-daily-p) 'value)人，
-                             @(hash-ref (fifth sorted-daily-p) 'name)：@(hash-ref (fifth sorted-daily-p) 'value)人。
-                             国外前五
-                             @(hash-ref (first sorted-daily-c) 'name)：@(hash-ref (first sorted-daily-c) 'value)人，
-                             @(hash-ref (second sorted-daily-c) 'name)：@(hash-ref (second sorted-daily-c) 'value)人，
-                             @(hash-ref (third sorted-daily-c) 'name)：@(hash-ref (third sorted-daily-c) 'value)人，
-                             @(hash-ref (fourth sorted-daily-c) 'name)：@(hash-ref (fourth sorted-daily-c) 'value)人，
-                             @(hash-ref (fifth sorted-daily-c) 'name)：@(hash-ref (fifth sorted-daily-c) 'value)人。
-                             })
-			    
-(define content @~a{
-                      @top5-con-today
-		      
-		      @overall
-		      
-		      @top5-con
-                      })
-
-(do-sending "新冠肺炎今日报告" content)
+                           『新增前五』
+                           @(hash-ref (first sorted-daily-p) 'name)：@(hash-ref (first sorted-daily-p) 'value)人，
+                           @(hash-ref (second sorted-daily-p) 'name)：@(hash-ref (second sorted-daily-p) 'value)人，
+                           @(hash-ref (third sorted-daily-p) 'name)：@(hash-ref (third sorted-daily-p) 'value)人，
+                           @(hash-ref (fourth sorted-daily-p) 'name)：@(hash-ref (fourth sorted-daily-p) 'value)人，
+                           @(hash-ref (fifth sorted-daily-p) 'name)：@(hash-ref (fifth sorted-daily-p) 'value)人。
+                           @(hash-ref (first sorted-daily-c) 'name)：@(hash-ref (first sorted-daily-c) 'value)人，
+                           @(hash-ref (second sorted-daily-c) 'name)：@(hash-ref (second sorted-daily-c) 'value)人，
+                           @(hash-ref (third sorted-daily-c) 'name)：@(hash-ref (third sorted-daily-c) 'value)人，
+                           @(hash-ref (fourth sorted-daily-c) 'name)：@(hash-ref (fourth sorted-daily-c) 'value)人，
+                           @(hash-ref (fifth sorted-daily-c) 'name)：@(hash-ref (fifth sorted-daily-c) 'value)人。
+                           })
 
 
-#;(define top5 @~a{
-               国内前五
-               @(hash-ref (first sorted-daily-p) 'name)：@(hash-ref (first sorted-daily-p) 'value)人，
-               @(hash-ref (second sorted-daily-p) 'name)：@(hash-ref (second sorted-daily-p) 'value)人，
-               @(hash-ref (third sorted-daily-p) 'name)：@(hash-ref (third sorted-daily-p) 'value)人，
-               @(hash-ref (fourth sorted-daily-p) 'name)：@(hash-ref (fourth sorted-daily-p) 'value)人，
-               @(hash-ref (fifth sorted-daily-p) 'name)：@(hash-ref (fifth sorted-daily-p) 'value)人。
-               国外前五
-               @(hash-ref (first sorted-daily-c) 'name)：@(hash-ref (first sorted-daily-c) 'value)人，
-               @(hash-ref (second sorted-daily-c) 'name)：@(hash-ref (second sorted-daily-c) 'value)人，
-               @(hash-ref (third sorted-daily-c) 'name)：@(hash-ref (third sorted-daily-c) 'value)人，
-               @(hash-ref (fourth sorted-daily-c) 'name)：@(hash-ref (fourth sorted-daily-c) 'value)人，
-               @(hash-ref (fifth sorted-daily-c) 'name)：@(hash-ref (fifth sorted-daily-c) 'value)人。
-               })
+(send-smtp-mail (make-mail "新冠肺炎今日报告"
+                           @~a{
+                               @overall
 
-#;(do-sending "新冠肺炎今日新增确诊报告" top5)
+                               @top5-con-today
 
+                               @top5-con
+                               }
+                           #:from (getenv "SENDER")
+                           #:to (string-split (getenv "RECIPIENT"))))
