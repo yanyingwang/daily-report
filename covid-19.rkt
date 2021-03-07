3fd632befd32e2645e41a99edf31bc9e9ebafabc
Yanying Wang <yanyingwang1@gmail.com>
2021-03-07 20:10:45 +0800

    fix string-join

--- covid-19.rkt
+++ covid-19.rkt
@@ -8,10 +8,12 @@
 
 (send-smtp-mail
  (make-mail "新冠肺炎报告"
-            (string-join qq/overall/china
-                         qq/top10/china
-                         sina/overall/china
-                         sina/top5/foreign
-                         sina/top5/today/foreign "\n")
+            (string-join 
+              (list qq/overall/china
+                    qq/top10/china
+                    sina/overall/china
+                    sina/top5/foreign
+                    sina/top5/today/foreign)
+              "\n")
             #:from (getenv "SENDER")
             #:to (string-split (getenv "RECIPIENT"))))
