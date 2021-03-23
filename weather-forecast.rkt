bcb44fcf7f7315336b14673596093a618fe41652
Yanying Wang <yanyingwang1@gmail.com>
2021-03-23 17:58:52 +0800

    fix for china mobile msg length limit

--- weather-forecast.rkt
+++ weather-forecast.rkt
@@ -13,14 +13,14 @@
 (define data/3d
   (hash-ref result/3d 'daily))
 
-(define sms/title "三日天气")
-(define sms/content
-  (string-join
-   (for/list ([d data/3d])
-     @~a{@(hash-ref d 'fxDate)：白天@(hash-ref d 'textDay)，夜间@(hash-ref d 'textNight)，气温@(hash-ref d 'tempMin)-@(hash-ref d 'tempMax)度。})
-   "\n"))
+(define data/formated-list
+  (for/list ([d data/3d])
+    (list @~a{@(substring (hash-ref d 'fxDate) 5 10)天气}
+          @~a{白天@(hash-ref d 'textDay)，夜间@(hash-ref d 'textNight)，气温@(hash-ref d 'tempMin)-@(hash-ref d 'tempMax)度。})))
 
-(send-smtp-mail
- (make-mail sms/title sms/content
-            #:from (getenv "SENDER")
-            #:to (string-split (getenv "RECIPIENT_CM"))))
+(for ([title-and-content data/formated-list])
+  (send-smtp-mail
+   (make-mail (car title-and-content) (cadr title-and-content)
+              #:from (getenv "SENDER")
+              #:to (string-split (getenv "RECIPIENT_CM"))))
+  (sleep 5))
