5dcc457a7836c519e266b067c5e93d4fc7be796c
Yanying Wang <yanyingwang1@gmail.com>
2021-03-25 17:43:52 +0800

    enh code logic

--- weather-forecast.rkt
+++ weather-forecast.rkt
@@ -13,14 +13,16 @@
 (define data/3d
   (hash-ref result/3d 'daily))
 
-(define data/formated-list
-  (for/list ([d data/3d])
-    (list @~a{@(substring (hash-ref d 'fxDate) 5 10)天气}
-          @~a{白天@(hash-ref d 'textDay)，夜间@(hash-ref d 'textNight)，气温@(hash-ref d 'tempMin)-@(hash-ref d 'tempMax)度。})))
+(define data/3d/filtered
+  (for/hash ([d data/3d])
+    (values @~a{@(substring (hash-ref d 'fxDate) 5 10)天气}
+            @~a{白天@(hash-ref d 'textDay)，夜间@(hash-ref d 'textNight)，气温@(hash-ref d 'tempMin)-@(hash-ref d 'tempMax)度。})))
 
-(for ([title-and-content data/formated-list])
+
+(for ([(title content) (in-hash dta/3d/filtered)])
   (send-smtp-mail
-   (make-mail (car title-and-content) (cadr title-and-content)
+   (make-mail title content
               #:from (getenv "SENDER")
               #:to (string-split (getenv "RECIPIENT_CM"))))
   (sleep 5))
+
