0566af6581acedb0042265eaaa60e75f33b366df
Yanying Wang <yanyingwang1@gmail.com>
2021-07-13 16:43:16 +0800

    enh gen of html with embed images

--- private/tools.rkt
+++ private/tools.rkt
@@ -1,6 +1,6 @@
 #lang racket/base
 
-(require racket/list)
+(require racket/list racket/file racket/path net/base64)
 (provide (all-defined-out))
 
 
@@ -9,14 +9,19 @@
         (h2 ((style "margin-bottom: 6px;")) ,(car h+ps))
         (p ((style "margin-top: 6px;")) ,@(add-between (cdr h+ps) '(br)))))
 
-(define (div-wrap-with-img h+ps img)
+(define (div-wrap-with-img h+ps img-file)
   `(div ((class "row"))
         (div ((class "text"))
          (h2 ((id "indent-10")) ,(car h+ps)))
         (div ((class "chart"))
-             (img ((src ,img) (alt "Nature") (class "responsive"))))
+             (img ((src ,(html-base64-en img-file)) (alt "Chart") (class "responsive"))))
         (div ((class "text"))
              (p  ,@(add-between (cdr h+ps) '(br))))))
+
+(define (html-base64-en f)
+  (format "data:image/~a;base64, ~a"
+          (subbytes (path-get-extension f) 1)
+          (base64-encode (file->bytes f))))
 
 (define (->plot-format lst)
   (for/list ([l lst])
