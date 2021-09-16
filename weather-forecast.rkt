7c839aae03cfc90f2be3f7963ea8f863490af021
Yanying Wang <yanyingwang@users.noreply.github.com>
2021-09-16 12:51:13 +0800

    Update weather-forecast.rkt

--- weather-forecast.rkt
+++ weather-forecast.rkt
@@ -8,13 +8,13 @@
 
 (define lid "101180106") ;;henan xinzheng
 ;; (define lid "101020100") ;;shanghai
-(define result/3d
-  (http-response-body (weather/3d lid)))
-(define data/3d
-  (hash-ref result/3d 'daily))
+(define result/nd
+  (http-response-body (weather/nd lid)))
+(define data/nd
+  (hash-ref result/nd 'daily))
 
-(define data/3d/filtered
-  (for/hash ([d data/3d])
+(define data/nd/filtered
+  (for/hash ([d data/nd])
     (values @~a{@(substring (hash-ref d 'fxDate) 5 7)/@(substring (hash-ref d 'fxDate) 8 10)天气}
             @~a{@(hash-ref d 'textDay)转@(hash-ref d 'textNight)，@(hash-ref d 'tempMin)-@(hash-ref d 'tempMax)度，@(hash-ref d 'windDirDay)@(hash-ref d 'windScaleDay)级。})))
 
