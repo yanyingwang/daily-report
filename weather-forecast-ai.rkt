72fb896ac3d852587324aad5e2fdd79d33fd3a1c
Yanying Wang <yanyingwang1@gmail.com>
2021-09-25 13:55:26 +0800

    add warning content

--- weather-forecast-ai.rkt
+++ weather-forecast-ai.rkt
@@ -12,11 +12,25 @@
   ;; "101070101" ;沈阳
   )
 
-(define content
+(define ai-content
   (weather/24h/severe-weather-ai lid))
 
-(unless (string-contains? content "24小时内无雨，请放心出行")
+(unless (string-contains? ai-content "24小时内无雨，请放心出行")
   (send-smtp-mail
    (make-mail "24小时内有雨！" content
               #:from (getenv "SENDER")
               #:to  (string-split (getenv "RECIPIENTS")))))
+
+
+(define warning-contents
+  (hash-ref (http-response-body (warning/now lid)) 'warning))
+
+(unless (empty? warning-contents)
+  (for ([i warning-contents])
+    (when (string=? (hash-ref i 'status) "active")
+      (sleep 10)
+      (send-smtp-mail
+       (make-mail (hash-ref i 'title)
+                  (hash-ref i 'text)
+                  #:from (getenv "SENDER")
+                  #:to  (string-split (getenv "RECIPIENTS")))))))
