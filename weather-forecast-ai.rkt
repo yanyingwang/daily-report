158c4193189d1c7b33445725b88d97228062fd42
Yanying Wang <yanyingwang1@gmail.com>
2022-02-22 18:22:35 +0800

    minorly enh ai content

--- weather-forecast-ai.rkt
+++ weather-forecast-ai.rkt
@@ -6,25 +6,27 @@
          (file "private/parameters.rkt"))
 
 (define lid
-  ;; "101230401" ;莆田
-  ;; "101020100" ;上海
-  "101180106" ;郑州
-  ;; "101070101" ;沈阳
+  ;; (cons "101230401" "莆田市")
+  ;; (cons "101020100" "上海市")
+  (cons "101180106" "新郑市")
+  ;; (cons "101180101" "郑州市")
+  ;; (cons "101070101" "沈阳市")
   )
 
 (define ai-content
-  (weather/24h/severe-weather-ai lid))
-
-(unless (string-contains? ai-content "24小时内无雨，请放心出行")
+  (weather/24h/severe-weather-ai (car lid)))
+(unless (string-contains? ai-content "24小时内无异常，请放心出行")
   (send-smtp-mail
-   (make-mail "24小时内有雨！" ai-content
+   (make-mail (if (string-contains? ai-content "雪")
+                  (string-append (cdr lid) "24小时内有雪！")
+                  (string-append (cdr lid) "24小时内有雨！"))
+              ai-content
               #:from (getenv "SENDER")
               #:to  (string-split (getenv "RECIPIENTS")))))
 
 
 (define warning-contents
-  (hash-ref (http-response-body (warning/now lid)) 'warning))
-
+  (hash-ref (http-response-body (warning/now (car lid))) 'warning))
 (unless (empty? warning-contents)
   (for ([i warning-contents])
     (when (string=? (hash-ref i 'status) "active")
