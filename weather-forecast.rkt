f7eaf1d60ba900fc2154e1aec1f56b9f70e1af94
Yanying Wang <yanyingwang1@gmail.com>
2022-02-22 18:09:12 +0800

    ghpage for weather and code of send emails

--- weather-forecast.rkt
+++ weather-forecast.rkt
@@ -3,7 +3,8 @@
 
 (require racket/string racket/format racket/list
          http-client qweather smtp
-         (file "private/parameters.rkt"))
+         (file "private/parameters.rkt")
+         (file "private/weather-forecast-genhtml.rkt"))
 
 
 (define lid "101180106") ;;henan xinzheng
@@ -18,10 +19,15 @@
     (values @~a{@(substring (hash-ref d 'fxDate) 5 7)/@(substring (hash-ref d 'fxDate) 8 10)天气}
             @~a{@(hash-ref d 'textDay)转@(hash-ref d 'textNight)，@(hash-ref d 'tempMin)~@(hash-ref d 'tempMax)度，@(hash-ref d 'windDirDay)@(string-replace (hash-ref d 'windScaleDay) "-" "~")级。})))
 
+
 (for ([(title content) (in-hash data/nd/filtered)])
   (send-smtp-mail
    (make-mail title content
               #:from (getenv "SENDER")
-              #:to (string-split (getenv "RECIPIENTS"))))
+              #:to (list (getenv "EMAIL3") (getenv "EMAIL4"))))
   (sleep 20))
 
+(send-smtp-mail
+ (make-mail "新郑市天气预报" (hash-ref xpages '新郑市)
+            #:from (getenv "SENDER")
+            #:to (list (getenv "EMAIL1") (getenv "EMAIL2"))))
