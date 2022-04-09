d1109e3d9737f66411ba9eeede9d893d0c8e17e8
Yanying Wang <yanyingwang1@gmail.com>
2022-04-09 22:24:20 +0800

    fix figure showing style

--- private/covid-19-qq.rkt
+++ private/covid-19-qq.rkt
@@ -17,10 +17,12 @@
         (list @~a{全国今日新增确诊：@(hash-ref (qq/data/china-add) 'confirm)人，}
               @~a{全国今日治愈：@(hash-ref (qq/data/china-add) 'heal)人，}
               @~a{全国今日死亡：@(hash-ref (qq/data/china-add) 'dead)人。}
-              @~a{河南今日新增确诊：@(qq/get-num* '河南)人，无症状@(qq/get-num* '河南 'wzz_add 'today)人。}
-              @~a{其中郑州：@(qq/get-num* '河南 #:city '郑州)人。}
-              @~a{上海今日新增确诊：@(qq/get-num* '上海)人，无症状@(qq/get-num* '上海 'wzz_add 'today)人，}
-              @~a{其中境外输入：@(qq/get-num* '上海 #:city '境外输入)人。})))
+              @~a{河南今日新增确诊：@(qq/get-num* '河南)人，}
+              @~a{河南今日新增无症状：@(qq/get-num* '河南 'wzz_add 'today)人，}
+              @~a{其中郑州确诊：@(qq/get-num* '河南 #:city '郑州)人。}
+              @~a{上海今日新增确诊：@(qq/get-num* '上海)人，}
+              @~a{上海今日新增无症状：@(qq/get-num* '上海 'wzz_add 'today)人，}
+              @~a{其中境外输入确诊：@(qq/get-num* '上海 #:city '境外输入)人。})))
 
 (define processed/domestic/top10
   (cons "国内前十（新增确诊）"
@@ -38,4 +40,4 @@
                              #:before-first "（其中"
                              #:after-last "）"
                              "，")))
-          @~a{@(car i)：确诊@(cdr i)人@|extra-str|，无症状@(qq/get-num* (car i) 'wzz_add 'today)人；})))
+          @~a{@(car i)：确诊@(cdr i)人@|extra-str|（无症状@(qq/get-num* (car i) 'wzz_add 'today)人）；})))
