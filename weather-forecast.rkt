f93f90753e3d4c3df44d373d6cd3cfae266cd065
Yanying Wang <yanyingwang1@gmail.com>
2024-07-10 13:12:29 +0800

    debug

--- weather-forecast.rkt
+++ weather-forecast.rkt
@@ -17,7 +17,9 @@
 
 ;-------
 
+(current-http-client/debug #t)
+
 (let ([t "新郑市天气预报"]
-      [m (weather/15d/ai (car xz))])
+      [m (weather/15d/ai (cdr xz))])
   (bark-xr t m)
   (nxq-weatherd-d t m))
